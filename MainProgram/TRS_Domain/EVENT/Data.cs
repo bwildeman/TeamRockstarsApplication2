62ccc6a912ee366171cf8116d08ce572adeca871
Ollie <ollievandeven@hotmail.nl>
2018-05-24 11:53:18 +0200

    Updated event class

--- MainProgram/TRS_Domain/EVENT/Data.cs
+++ MainProgram/TRS_Domain/EVENT/Data.cs
@@ -6,17 +6,22 @@ namespace TRS_Domain.EVENT
 {
     public class Data
     {
-        public int EventId { get; private set; }
+        public int Id { get; private set; }
+        public int GroupId { get; private set; }
         public string Name { get; private set; }
+        public DateTime Date { get; private set; }
         public bool Online { get; private set; }
         public string Location { get; private set; }
         public string Description { get; private set; }
-        public List<CATEGORY.Data> Interests { get; private set; }
 
-        public Data(int eventId, string name, string description)
+        public Data(int id, int groupId, string name, DateTime date, bool online, string location, string description)
         {
-            EventId = eventId;
+            Id = id;
+            GroupId = groupId;
             Name = name;
+            Date = date;
+            Online = online;
+            Location = location;
             Description = description;
         }
 
