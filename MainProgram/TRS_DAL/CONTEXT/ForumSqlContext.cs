f3e7ab04a1adc55aa4ed829a43b1a8c0bd05b1d4
bwildeman <36735028+bwildeman@users.noreply.github.com>
2018-05-30 15:30:40 +0200

    Started inplementing forums

---
+++ MainProgram/TRS_DAL/CONTEXT/ForumSqlContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TRS_DAL.INTERFACES;
+
+namespace TRS_DAL.CONTEXT
+{
+    public class ForumSqlContext : IForumContext
+    {
+        public List<DataRow> GetGroupNames(int groupID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> GetGroups(int userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewComment(int userId, int postId, string comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewForum(string forumName, int groupID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewPost(int forumID, int userID, string title, string postText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> SelectAllPostData(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> SelectComments(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> SelectCommentVotes(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> SelectForums(int groupID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> SelectPostVotes(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVote(int VoteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> UserHasVotedComment(int userId, int postId, int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataRow> UserHasVotedPost(int userId, int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VoteComment(int userId, int postId, int commentId, int VoteValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VotePost(int userId, int postId, int VoteValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainProgram/TRS_DAL/REPOSITORIES/ForumRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TRS_DAL.CONTEXT;
+using TRS_DAL.INTERFACES;
+
+namespace TRS_DAL.REPOSITORIES
+{
+    public class ForumRepository
+    {
+        IForumContext forumContext = new ForumSqlContext();
+        public void NewPost(int forumID, int userID, string title, string postText)
+        {
+            forumContext.NewPost(forumID, userID, title, postText);
+        }
+
+        public void NewComment(int userId, int postId, string comment)
+        {
+            forumContext.NewComment(userId,postId,comment);
+        }
+
+        public void NewForum(string forumName, int groupID)
+        {
+            forumContext.NewForum(forumName,groupID);
+        }
+
+        public List<DataRow> SelectAllPostData(int forumId)
+        {
+            return forumContext.SelectAllPostData(forumId);
+        }
+
+        public List<DataRow> SelectPostVotes(int forumId)
+        {
+            return forumContext.SelectPostVotes(forumId);
+        }
+
+        public List<DataRow> SelectCommentVotes(int postId)
+        {
+            return forumContext.SelectCommentVotes(postId);
+        }
+
+        public List<DataRow> SelectForums(int groupID)
+        {
+            return forumContext.SelectForums(groupID);
+        }
+
+        public List<DataRow> SelectComments(int postId)
+        {
+            return forumContext.SelectComments(postId);
+        }
+
+        public void VotePost(int userId, int postId, int VoteValue)
+        {
+            forumContext.VotePost(userId,postId,VoteValue);
+        }
+
+        public void VoteComment(int userId, int postId, int commentId, int VoteValue)
+        {
+            forumContext.VoteComment(userId,postId,commentId,VoteValue);
+        }
+
+        public void UpdateVote(int VoteId)
+        {
+            forumContext.UpdateVote(VoteId);
+        }
+
+        public List<DataRow> UserHasVotedPost(int userId, int postId)
+        {
+            return forumContext.UserHasVotedPost(userId,postId);
+        }
+
+        public List<DataRow> UserHasVotedComment(int userId, int postId, int commentId)
+        {
+            return forumContext.UserHasVotedComment(userId,postId,commentId);
+        }
+
+        public List<DataRow> GetGroups(int userID)
+        {
+            return forumContext.GetGroups(userID);
+            //TODO move to group sql
+        }
+
+        public List<DataRow> GetGroupNames(int groupID)
+        {
+            return forumContext.GetGroupNames(groupID);
+            //TODO move to group sql
+        }
+    }
+}
