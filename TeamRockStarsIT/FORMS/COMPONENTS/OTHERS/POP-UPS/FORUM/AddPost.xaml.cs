2d778086af3109e07bfc9b2fefbbb934db3715dc
lfb0801 <lfb0801@gmail.com>
2018-06-07 10:53:51 +0200

    adding event UI

---
+++ TeamRockStarsIT/FORMS/COMPONENTS/OTHERS/POP-UPS/FORUM/AddPost.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamRockStarsIT.FORMS.COMPONENTS.OTHERS.POP_UPS.FORUM
+{
+    /// <summary>
+    /// Interaction logic for AddPost.xaml
+    /// </summary>
+    public partial class AddPost : Window
+    {
+        public AddPost()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertPost_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
