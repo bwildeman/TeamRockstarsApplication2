b01c925b6a24a4a5d6572bc9ef77341bbae852b7
Marco Driessen <m.driessen@student.fontys.nl>
2018-06-12 12:15:40 +0200

    Banner update
    Created banner

--- MainProgram/TRS_Logic/ClientClass.cs
+++ MainProgram/TRS_Logic/ClientClass.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using System.Windows.Forms;
 
 
 //using MySql.Data.MySqlClient;
@@ -30,7 +29,7 @@ namespace TRS_Logic
         public void LoadIn()
         {
 
-            string ip = "145.93.112.130";
+            string ip = "145.93.64.66";
 
 
             master = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
